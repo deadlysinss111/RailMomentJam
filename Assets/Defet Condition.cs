5f5a17719cb3985f8ee5ce05312387a99ba2c533
G-tech-C2H6 <efaure@gaming.tech>
2025-02-15 17:24:48 +0100

    Clean-up

--- Assets/Defet Condition.cs
+++ Assets/Defet Condition.cs
@@ -5,28 +5,5 @@ using UnityEngine.SceneManagement;
 
 public class DefeatCondition : MonoBehaviour
 {
-    public string playerTag = "Player Life";
-    public string defeatSceneName = "DefeatScene";
 
-    private GameObject playerLifeObject;
-
-    void Start()
-    {
-
-        playerLifeObject = GameObject.FindGameObjectWithTag(playerTag);
-
-        if (playerLifeObject == null)
-        {
-            Debug.LogError("Aucun objet avec le tag 'Player collision' n'a été trouvé !");
-        }
-    }
-
-    void Update()
-    {
-        if (playerLifeObject == null)
-        {
-            Debug.Log("About to laod scene");
-            SceneManager.LoadScene(defeatSceneName, LoadSceneMode.Single);
-        }
-    }
 }
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,17 +6,35 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+
+    public string playerTag = "Player Life";
+    public string defeatSceneName = "DefeatScene";
     [NonSerialized] static public Action gameOver;
-
+    private GameObject playerLifeObject;
     [SerializeField] string loseSceneName;
-
-    private void Awake()
-    {
-        gameOver += OnLose;
-    }
 
     void OnLose()
     {
         SceneManager.LoadScene(loseSceneName);
+    }
+
+    void Start()
+    {
+        gameOver += OnLose;
+        playerLifeObject = GameObject.FindGameObjectWithTag(playerTag);
+
+        if (playerLifeObject == null)
+        {
+            Debug.LogError("Aucun objet avec le tag 'Player collision' n'a été trouvé !");
+        }
+    }
+
+    void Update()
+    {
+        if (playerLifeObject == null)
+        {
+            Debug.Log("About to laod scene");
+            SceneManager.LoadScene(defeatSceneName, LoadSceneMode.Single);
+        }
     }
 }
