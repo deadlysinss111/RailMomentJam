5adda1cb0ae3290cfe535f310803e39fa9c81535
Romain Boone <rboone@gaming.tech>
2025-02-15 18:24:44 +0100

    spawn balloon

---
+++ Assets/LevelChunks/Coaster/ChunksPrefab/Balloon.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class Balloon : MonoBehaviour
+{
+    public float liftSpeed = 1f;
+    public float lifetime = 8f;
+    private float timer = 0f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.Translate(new Vector3(0,1,0) /** liftSpeed*/ * Time.deltaTime);
+
+        timer += Time.deltaTime;
+
+        if (timer >= lifetime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/LevelChunks/Coaster/ChunksPrefab/SpawnWithCollide.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnWithCollide : MonoBehaviour
+{
+    [SerializeField] private GameObject objectToBeSpawned = null;
+    [SerializeField] public PlayerManager playerManager = null;
+    public float speed = 10f;
+    private int nbBalloon = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"Collide detecte with {other.name}");
+
+        if (other.gameObject.CompareTag("Rider"))
+        { 
+            for (int i = 0; i < nbBalloon; i++)
+            {
+                float radius = 2f; // Rayon autour du point
+                Vector3 randomOffset = Random.insideUnitSphere * radius;
+
+                GameObject ball = Instantiate(
+                    objectToBeSpawned,
+                    transform.position + new Vector3(0, 7, 20) + randomOffset,
+                    Quaternion.identity
+                );
+            }            
+            Debug.Log("Spawn de balle");
+            Destroy(gameObject);
+        }
+       
+    }
+}
