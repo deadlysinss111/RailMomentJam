f2e5ab786b1c9e5b15ec7c7b6466f18bc73dc23e
G-tech-C2H6 <efaure@gaming.tech>
2025-02-16 09:17:29 +0100

    Level 1 Ethan

--- Assets/Player/PosCamera.cs
+++ Assets/Player/PosCamera.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PosCamera : MonoBehaviour
 {
+    public float posX, posY = 1, posZ;
     // Start is called before the first frame update
     void Start()
     {
-        transform.position += new Vector3(0, 1, 0);
+        transform.position += new Vector3(posX, posY, posZ);
     }
 
     // Update is called once per frame
