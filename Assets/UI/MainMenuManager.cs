8ec3cddbbd8f5b0bc456e9899247a07e790351bf
Baptiste-GC-Tech <117664376+Baptiste-GC-Tech@users.noreply.github.com>
2025-02-16 07:58:30 +0100

    Tried fixing UI

--- Assets/UI/MainMenuManager.cs
+++ Assets/UI/MainMenuManager.cs
@@ -1,10 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.Android.Gradle.Manifest;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    /*
+     *  DEBUG
+     */
+    public TextMeshProUGUI DEBUGscreenSize;
+    private void Start()
+    {
+        Vector2Int screenSize = new Vector2Int();
+        screenSize.x = UnityEngine.Screen.width;
+        screenSize.y = UnityEngine.Screen.height;
+
+        DEBUGscreenSize.text = screenSize.ToString();
+    }
+
+
+
+
     /*
      *  METHODS
      */
