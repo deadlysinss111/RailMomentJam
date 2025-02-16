e3164ab853af3845cee9f8dd2e094fd4ca1f7536
deadlysinss111 <vgautier@gaming.tech>
2025-02-16 07:16:00 +0100

    build tuff

--- Assets/Player/PlayerManager.cs
+++ Assets/Player/PlayerManager.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class PlayerManager : MonoBehaviour
 {
